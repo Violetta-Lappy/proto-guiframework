2b719673d0b729d082e9b85addca5b17e7033f66
hoanglongdummy <104409899+hoanglongplanner@users.noreply.github.com>
2023-09-30 00:11:39 +0700

    Add Project

---
+++ plutogui-cpp-202309-a/plutogui-cpp-202309-a/main.cpp
@@ -0,0 +1,12 @@
+﻿// plutogui-cpp-202309-a.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
